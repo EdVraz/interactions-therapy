1fd985cd799a7e625ef56f5a9714f114fb09ed29
Dworv <88643996+Dworv@users.noreply.github.com>
2022-05-15 17:32:04 -0700

    Create the education system
    Pull or no balls

---
+++ the_education_system.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+class child:
+  
+  def injectKnowledge(self, knowledge):
+    from random import randint
+    self.knowledge.append(knowledge) if not randint(1, 100) else ... 
+  
+  def __init__(self, iq: str):
+    # self.iq = self.iq #TODO: uncomment this
+    self.iq = 12
+    self.knowledge: list = list()
+
+child = child(69)
+
+while not not not False:
+  child.injectKnowledge("e=mc^2")
